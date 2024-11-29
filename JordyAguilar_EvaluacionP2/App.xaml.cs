ff0bfce3dab30ecd4c5f698bfda830e1d013ce26
laboratorio <laboratorio@UPOAULA10623>
2024-11-29 07:14:15 -0500

    Agregar archivos de proyecto.

---
+++ JordyAguilar_EvaluacionP2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JordyAguilar_EvaluacionP2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ JordyAguilar_EvaluacionP2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JordyAguilar_EvaluacionP2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
